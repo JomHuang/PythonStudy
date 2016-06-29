e26c9ece71fbce32af7736818e3e1a0bd3569907
huangkaifa <159004287@qq.com>
2016-06-29 23:10:47 +0800

    数据库

--- Day_1/Hello.py
+++ Day_1/Hello.py
@@ -1,3 +1,15 @@
-print("hello!")
-print(1+2)
-print(2+3)
+"""
+sqlite
+"""
+sqlite3_PATH = "./DataFiles/somedatabase.db";
+import sqlite3;
+
+#创建数据连接,如果文件不存在就创建一个新的
+conn = sqlite3.connect(sqlite3_PATH);
+#获得连接的游标,这个游标可以用来执行SQL查询
+curs=conn.cursor();
+
+#提交
+conn.commit();
+#关闭
+conn.close();
